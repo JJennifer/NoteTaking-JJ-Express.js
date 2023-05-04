e7a9d2441e222d97b87f8f554d0f25aad0d04cab
JJ <jenniferj1796@gmail.com>
2023-05-03 23:48:47 -0400

    changed order

--- server.js
+++ server.js
@@ -13,14 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => 
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+// app.get("*", (req, res) => 
+//     res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
 
 // the /notes is given by the index.js line 7 
 app.get("/notes", (req, res) => 
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
 
 // /api/notes is given by index.js line 29
 app.get("/api/notes", (req, res) => {
@@ -33,6 +34,10 @@ app.get("/api/notes", (req, res) => {
         }
     })
 });
+
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 // grabing user input to store
 app.post("/api/notes", (req, res) => {
