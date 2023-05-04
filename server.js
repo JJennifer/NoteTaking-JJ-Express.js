83d13693ec87a0041fe8d90ca35e1d08ef23a99f
JJ <jenniferj1796@gmail.com>
2023-05-03 22:28:05 -0400

    fixed bug and added comments

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const fs = require("fs");
 const db = require("./db/db.json")
+
 
 const PORT = process.env.Port || 3001;
 
@@ -10,18 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
+// the /notes is given by the index.js line 7 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-
-app.get("./api/notes", (req, res) => {
+// /api/notes is given by index.js line 29
+app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (error, data) => {
         if (error) {
             console.log("There is an error");
@@ -32,7 +34,9 @@ app.get("./api/notes", (req, res) => {
     })
 });
 
+// grabing user input to store
 app.post("/api/notes", (req, res) => {
+    // title and text was given in db.json
     const {title, text} = req.body;
     if(title && text){
         const newNote = {
@@ -40,13 +44,14 @@ app.post("/api/notes", (req, res) => {
             text,
         };
     };
+    // reading notes in system to list them along with new note
     fs.readFile("./db/db.json", "utf8", (error, data)=> {
         if(error){
             console.log("There is an error");
         } else {
             const datanotes = JSON.parse(data);
             datanotes.push(newNote);
-            
+            // listing the new note
             fs.writeFile("./db/db.json", JSON.stringify(datanotes), (error) => {
                 if(error){
                     console.log("There is an error");
